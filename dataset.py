3232a175ee6293e6a75d6280bf87e81238a2a5d7
cilikinari <cilikinari2005@gmail.com>
2026-04-29 21:36:37 +0800

    fix: remove data augmentation transformations from dataloader

--- dataset.py
+++ dataset.py
@@ -8,12 +8,6 @@ def get_dataloader(batch_size):
     # transform
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
-
-        # 🔥 AUGMENTATION
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(10),
-        transforms.ColorJitter(brightness=0.2, contrast=0.2),
-
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5),
                             (0.5, 0.5, 0.5))
